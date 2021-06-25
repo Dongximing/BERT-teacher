fe33b6b7e9f4e5921c014b2832d389d5f162433c
Ximing <dongxx1104@gmail.com>
2021-06-25 15:44:03 -0500

    fix

--- earlystopping.py
+++ earlystopping.py
@@ -4,7 +4,7 @@ import configs
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0, path=config.MODEL_PATH, trace_func=print):
+    def __init__(self, patience=7, verbose=False, delta=0, path=configs.MODEL_PATH, trace_func=print):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
